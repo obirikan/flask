f2cd751081071ac755021424cc90f97ab33deca4
obirikan <obirikan020@gmail.com>
2022-10-13 01:59:45 +0000

    -

--- flas.py
+++ flas.py
@@ -1,12 +1,12 @@
 from distutils.log import debug
-from flask import Flask
+from flask import Flask,request,jsonify
 from flask_restful import Api,Resource,reqparse
 
 #enables you to make put request with ease
 args=reqparse.RequestParser()
 
 #request arguements 
-args.add_argument('name',type=str, help='err')
+# args.add_argument('name',type=str, help='err')
 
 #insantiate varibles to enable create enpoits
 app = Flask(__name__)
@@ -14,11 +14,11 @@ api= Api(app)
 
 #this is where you instantiate the endpoints
 class ml(Resource):
-  def put(self):
-      arr=args.parse_args()
-      return arr
+  def post(self):
+    data = request.json
+    return jsonify(data)
 
-#this enables the api endpoits
+#this enables the api endpoints
 api.add_resource(ml,'/')
 
 
