fd15b30166d97c4bf4d6b68f10aa375bf117e83e
obirikan <obirikan020@gmail.com>
2022-10-19 13:17:17 +0000

    added ml model

--- flas.py
+++ flas.py
@@ -1,6 +1,8 @@
 from distutils.log import debug
+import numpy as np
 from flask import Flask,request,jsonify
 from flask_restful import Api,Resource,reqparse
+import pickle
 
 #enables you to make put request with ease
 args=reqparse.RequestParser()
@@ -16,7 +18,17 @@ api= Api(app)
 class ml(Resource):
   def post(self):
     data = request.json
-    return jsonify(data)
+    savedmodel=open('studentmodel.pickle', 'rb')
+    newlinear=pickle.load(savedmodel)
+    mine=[[5,401,4,2,16,2,16,8,2500]]
+    mine=np.array(mine)
+    #predict the outcome of your value(s)
+    predictions=newlinear.predict(mine)
+
+#loop through prediction to see if your data is corresponding well
+    for x in range(len(predictions)):
+        print(predictions[x])
+        return jsonify(predictions[x])
 
 #this enables the api endpoints
 api.add_resource(ml,'/')
