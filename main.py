418fd340b1afda36cfd80da998513624d8484441
kelvin obiri <92574631+obirikan@users.noreply.github.com>
2022-10-27 15:40:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-from flask import Flask, jsonify
-import os
+from distutils.log import debug
+import json
+import numpy as np
+from flask_cors import CORS
+from flask import Flask,request,jsonify
+import pickle
 
 app = Flask(__name__)
+CORS(app)
+
+@app.route('/get',methods =['POST'])
+def post_details():
+    data = request.json
+    mine=[data['name']]
+    mine=np.array(mine)
+    savedmodel=open('heartprediction.pickle', 'rb')
+    newmodel=pickle.load(savedmodel)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    myvalues=[[20, 0 ,3, 100 ,100,0,1,100]]
+    
+    #predict the outcome of your value(s)
+    predicted=newmodel.predict(mine)
+    names=['absense','presence']
+    
+#loop through prediction to see if your data is corresponding well
+    for x in range(len(predicted)):
+        return jsonify(names[predicted[x]])
 
-
-if __name__ == '__main__':
+#this starts the server
+if __name__ == "__main__":
     app.run(debug=True, port=os.getenv("PORT", default=5000))
